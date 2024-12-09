66b961ec48de8a65d204afbd3260be48d730b7d2
sohel <sohelf131@gmail.com>
2024-12-09 13:26:57 +0600

    initial setip express server and connect mongoDB

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const cors = require("cors");
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// MongoDB connection
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB connected"))
+  .catch((err) => console.error(err));
+
+// Routes
+app.get("/", (req, res) => {
+    res.send("ok")
+})
+
+app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
